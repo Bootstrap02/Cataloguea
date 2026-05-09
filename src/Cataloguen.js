e31013b67857087ca6cc89d97310a0bc766cb147
Louis Joseph <143167253+Bootstrap02@users.noreply.github.com>
2026-05-09 09:47:06 +0100

    Update Cataloguen.js

--- src/Cataloguen.js
+++ src/Cataloguen.js
@@ -351,7 +351,7 @@ const Homepage = () => {
 
     /* Calculate total stakes from all chain assets (winner + specials) */
     const totalStakes = Object.values(gameStakes).reduce((sum, val) => sum + (val || 0), 0);
-    const winner = badgamedeficit - gameStakes.winner
+    const winner = badGamesDeficit - gameStakes.winner
     /* FIX: On no-win, badGamesDeficit PLUS totalStakes goes to martingaleDeficit */
     if (!smallTeamImpact) {
       // No win happened - push the full chain total + all stakes forward
