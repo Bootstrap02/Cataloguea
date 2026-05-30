f221a7f1f66b7b7471259a90e7ea675b95268f54
Louis Ogochukwu Udegbue <143167253+Bootstrap02@users.noreply.github.com>
2026-05-30 09:26:55 +0100

    Update Cataloguec.js

--- src/Cataloguec.js
+++ src/Cataloguec.js
@@ -367,6 +367,7 @@ const Homepage = () => {
     setZeroAmounts({ winnerAmount:0, homeAmount:0, drawAmount:0, awayAmount:0 });
     setOneAmounts({ winnerAmount:0, homeAmount:0, drawAmount:0, awayAmount:0 });
     saveBase();
+    console.log(orderedStakes);
   };
 
   const teamA = sanitizeTeam(inputA) || "HME";
