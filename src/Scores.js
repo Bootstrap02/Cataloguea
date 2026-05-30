316fca95251faca22f078d69b4dbf90c9c8ad959
Louis Ogochukwu Udegbue <143167253+Bootstrap02@users.noreply.github.com>
2026-05-30 17:56:29 +0100

    Update Scores.js

--- src/Scores.js
+++ src/Scores.js
@@ -21,26 +21,27 @@ export const odds = [
 
 
 
+
 export const smallOdds = [
-  { id: "livcry", home: "liv", away: "cry", winner: 105, oneX: 20.4, twoX: 20.4, x1: 6, zeroGoals: 14.7, sixGoals: 15.5, ht11: 6.8, O45: 5.8, htgg: 4.3, ft11: 8.5, wingg: 3.8, fourGoals: 5.3 },
-  { id: "livbou", home: "liv", away: "bou", winner: 105, oneX: 20.4, twoX: 20.4, x1: 6, zeroGoals: 14.7, sixGoals: 15.5, ht11: 6.8, O45: 5.8, htgg: 4.3, ft11: 8.5, wingg: 3.8, fourGoals: 5.3 },
-  { id: "livful", home: "liv", away: "ful", winner: 105, oneX: 20.4, twoX: 20.4, x1: 6, zeroGoals: 14.7, sixGoals: 15.5, ht11: 6.8, O45: 5.8, htgg: 4.3, ft11: 8.5, wingg: 3.8, fourGoals: 5.3 },
-  { id: "livwhu", home: "liv", away: "whu", winner: 66.1, oneX: 24.9, twoX: 24.9, x1: 6, zeroGoals: 17.7, sixGoals: 13, ht11: 6.8, O45: 5.8, htgg: 4.3, ft11: 8.5, wingg: 3.8, fourGoals: 5.3 },
-  { id: "livbre", home: "liv", away: "bre", winner: 66.1, oneX: 24.9, twoX: 24.9, x1: 6, zeroGoals: 17.7, sixGoals: 13, ht11: 6.8, O45: 5.8, htgg: 4.3, ft11: 8.5, wingg: 3.8, fourGoals: 5.3 },
-  { id: "livwol", home: "liv", away: "wol", winner: 66.1, oneX: 24.9, twoX: 24.9, x1: 6, zeroGoals: 17.7, sixGoals: 13, ht11: 6.8, O45: 5.8, htgg: 4.3, ft11: 8.5, wingg: 3.8, fourGoals: 5.3 },
-  { id: "livlee", home: "liv", away: "lee", winner: 66.1, oneX: 24.9, twoX: 24.9, x1: 6, zeroGoals: 17.7, sixGoals: 13, ht11: 6.8, O45: 5.8, htgg: 4.3, ft11: 8.5, wingg: 3.8, fourGoals: 5.3 },
-  { id: "liveve", home: "liv", away: "eve", winner: 66.1, oneX: 24.9, twoX: 24.9, x1: 6, zeroGoals: 17.7, sixGoals: 13, ht11: 6.8, O45: 5.8, htgg: 4.3, ft11: 8.5, wingg: 3.8, fourGoals: 5.3 },
-  { id: "livsun", home: "liv", away: "sun", winner: 46.3, oneX: 31.2, twoX: 31.2, x1: 6, zeroGoals: 16.6, sixGoals: 13.3, ht11: 6.8, O45: 5.8, htgg: 4.3, ft11: 8.5, wingg: 3.8, fourGoals: 5.3 },
-  { id: "livbur", home: "liv", away: "bur", winner: 46.3, oneX: 31.2, twoX: 31.2, x1: 6, zeroGoals: 16.6, sixGoals: 13.3, ht11: 6.8, O45: 5.8, htgg: 4.3, ft11: 8.5, wingg: 3.8, fourGoals: 5.3 },
-  { id: "livast", home: "liv", away: "ast", winner: 229, oneX: 16.6, twoX: 16.6, x1: 6, zeroGoals: 16, sixGoals: 17.2, ht11: 6.8, O45: 5.8, htgg: 4.3, ft11: 8.5, wingg: 3.8, fourGoals: 5.3 },
-  { id: "livtot", home: "liv", away: "tot", winner: 229, oneX: 16.6, twoX: 16.6, x1: 6, zeroGoals: 16, sixGoals: 17.2, ht11: 6.8, O45: 5.8, htgg: 4.3, ft11: 8.5, wingg: 3.8, fourGoals: 5.3 },
-  { id: "livbha", home: "liv", away: "bha", winner: 144, oneX: 18.6, twoX: 18.6, x1: 6, zeroGoals: 16, sixGoals: 15.1, ht11: 6.8, O45: 5.8, htgg: 4.3, ft11: 8.5, wingg: 3.8, fourGoals: 5.3 },
-  { id: "livnot", home: "liv", away: "not", winner: 144, oneX: 18.6, twoX: 18.6, x1: 6, zeroGoals: 16, sixGoals: 15.1, ht11: 6.8, O45: 5.8, htgg: 4.3, ft11: 8.5, wingg: 3.8, fourGoals: 5.3 },
-  { id: "whuliv", home: "whu", away: "liv", winner: 262, oneX: 19.4, twoX: 19.4, x1: 6, zeroGoals: 18, sixGoals: 17.1, ht11: 6.8, O45: 5.8, htgg: 4.3, ft11: 8.5, wingg: 3.8, fourGoals: 5.3 },
-  { id: "breliv", home: "bre", away: "liv", winner: 262, oneX: 19.4, twoX: 19.4, x1: 6, zeroGoals: 18, sixGoals: 17.1, ht11: 6.8, O45: 5.8, htgg: 4.3, ft11: 8.5, wingg: 3.8, fourGoals: 5.3 },
-  { id: "wolliv", home: "wol", away: "liv", winner: 262, oneX: 19.4, twoX: 19.4, x1: 6, zeroGoals: 18, sixGoals: 17.1, ht11: 6.8, O45: 5.8, htgg: 4.3, ft11: 8.5, wingg: 3.8, fourGoals: 5.3 },
-  { id: "leeliv", home: "lee", away: "liv", winner: 262, oneX: 19.4, twoX: 19.4, x1: 6, zeroGoals: 18, sixGoals: 17.1, ht11: 6.8, O45: 5.8, htgg: 4.3, ft11: 8.5, wingg: 3.8, fourGoals: 5.3 },
-  { id: "eveliv", home: "eve", away: "liv", winner: 262, oneX: 19.4, twoX: 19.4, x1: 6, zeroGoals: 18, sixGoals: 17.1, ht11: 6.8, O45: 5.8, htgg: 4.3, ft11: 8.5, wingg: 3.8, fourGoals: 5.3 },
-  { id: "burliv", home: "bur", away: "liv", winner: 168, oneX: 22.3, twoX: 22.3, x1: 6, zeroGoals: 15.3, sixGoals: 17.4, ht11: 6.8, O45: 5.8, htgg: 4.3, ft11: 8.5, wingg: 3.8, fourGoals: 5.3 },
-  { id: "sunliv", home: "sun", away: "liv", winner: 168, oneX: 22.3, twoX: 22.3, x1: 6, zeroGoals: 15.3, sixGoals: 17.4, ht11: 6.8, O45: 5.8, htgg: 4.3, ft11: 8.5, wingg: 3.8, fourGoals: 5.3 }
+  { id: "livcry", home: "liv", away: "cry", winner: 105, fiveZero: 40.4, fiveOne: 45.5, oneX: 20.4, twoX: 20.4, x1: 6, zeroGoals: 14.7, sixGoals: 15.5, ht11: 6.8, O45: 5.8, htgg: 4.3, ft11: 8.5, wingg: 3.8, fourGoals: 5.3 },
+  { id: "livbou", home: "liv", away: "bou", winner: 105, fiveZero: 40.4, fiveOne: 45.5, oneX: 20.4, twoX: 20.4, x1: 6, zeroGoals: 14.7, sixGoals: 15.5, ht11: 6.8, O45: 5.8, htgg: 4.3, ft11: 8.5, wingg: 3.8, fourGoals: 5.3 },
+  { id: "livful", home: "liv", away: "ful", winner: 105, fiveZero: 40.4, fiveOne: 45.5, oneX: 20.4, twoX: 20.4, x1: 6, zeroGoals: 14.7, sixGoals: 15.5, ht11: 6.8, O45: 5.8, htgg: 4.3, ft11: 8.5, wingg: 3.8, fourGoals: 5.3 },
+  { id: "livwhu", home: "liv", away: "whu", winner: 66.1, fiveZero: 28, fiveOne: 33.9, oneX: 24.9, twoX: 24.9, x1: 6, zeroGoals: 17.7, sixGoals: 13, ht11: 6.8, O45: 5.8, htgg: 4.3, ft11: 8.5, wingg: 3.8, fourGoals: 5.3 },
+  { id: "livbre", home: "liv", away: "bre", winner: 66.1, fiveZero: 28, fiveOne: 33.9, oneX: 24.9, twoX: 24.9, x1: 6, zeroGoals: 17.7, sixGoals: 13, ht11: 6.8, O45: 5.8, htgg: 4.3, ft11: 8.5, wingg: 3.8, fourGoals: 5.3 },
+  { id: "livwol", home: "liv", away: "wol", winner: 66.1, fiveZero: 28, fiveOne: 33.9, oneX: 24.9, twoX: 24.9, x1: 6, zeroGoals: 17.7, sixGoals: 13, ht11: 6.8, O45: 5.8, htgg: 4.3, ft11: 8.5, wingg: 3.8, fourGoals: 5.3 },
+  { id: "livlee", home: "liv", away: "lee", winner: 66.1, fiveZero: 28, fiveOne: 33.9, oneX: 24.9, twoX: 24.9, x1: 6, zeroGoals: 17.7, sixGoals: 13, ht11: 6.8, O45: 5.8, htgg: 4.3, ft11: 8.5, wingg: 3.8, fourGoals: 5.3 },
+  { id: "liveve", home: "liv", away: "eve", winner: 66.1, fiveZero: 28, fiveOne: 33.9, oneX: 24.9, twoX: 24.9, x1: 6, zeroGoals: 17.7, sixGoals: 13, ht11: 6.8, O45: 5.8, htgg: 4.3, ft11: 8.5, wingg: 3.8, fourGoals: 5.3 },
+  { id: "livsun", home: "liv", away: "sun", winner: 46.3, fiveZero: 20.6, fiveOne: 31.9, oneX: 31.2, twoX: 31.2, x1: 6, zeroGoals: 16.6, sixGoals: 13.3, ht11: 6.8, O45: 5.8, htgg: 4.3, ft11: 8.5, wingg: 3.8, fourGoals: 5.3 },
+  { id: "livbur", home: "liv", away: "bur", winner: 46.3, fiveZero: 20.6, fiveOne: 31.9, oneX: 31.2, twoX: 31.2, x1: 6, zeroGoals: 16.6, sixGoals: 13.3, ht11: 6.8, O45: 5.8, htgg: 4.3, ft11: 8.5, wingg: 3.8, fourGoals: 5.3 },
+  { id: "livast", home: "liv", away: "ast", winner: 229, fiveZero: 71.2, fiveOne: 67.3, oneX: 16.6, twoX: 16.6, x1: 6, zeroGoals: 16, sixGoals: 17.2, ht11: 6.8, O45: 5.8, htgg: 4.3, ft11: 8.5, wingg: 3.8, fourGoals: 5.3 },
+  { id: "livtot", home: "liv", away: "tot", winner: 229, fiveZero: 71.2, fiveOne: 67.3, oneX: 16.6, twoX: 16.6, x1: 6, zeroGoals: 16, sixGoals: 17.2, ht11: 6.8, O45: 5.8, htgg: 4.3, ft11: 8.5, wingg: 3.8, fourGoals: 5.3 },
+  { id: "livbha", home: "liv", away: "bha", winner: 144, fiveZero: 52, fiveOne: 51.3, oneX: 18.6, twoX: 18.6, x1: 6, zeroGoals: 16, sixGoals: 15.1, ht11: 6.8, O45: 5.8, htgg: 4.3, ft11: 8.5, wingg: 3.8, fourGoals: 5.3 },
+  { id: "livnot", home: "liv", away: "not", winner: 144, fiveZero: 52, fiveOne: 51.3, oneX: 18.6, twoX: 18.6, x1: 6, zeroGoals: 16, sixGoals: 15.1, ht11: 6.8, O45: 5.8, htgg: 4.3, ft11: 8.5, wingg: 3.8, fourGoals: 5.3 },
+  { id: "whuliv", home: "whu", away: "liv", winner: 262, fiveZero: 78.7, fiveOne: 68.8, oneX: 19.4, twoX: 19.4, x1: 6, zeroGoals: 18, sixGoals: 17.1, ht11: 6.8, O45: 5.8, htgg: 4.3, ft11: 8.5, wingg: 3.8, fourGoals: 5.3 },
+  { id: "breliv", home: "bre", away: "liv", winner: 262, fiveZero: 78.7, fiveOne: 68.8, oneX: 19.4, twoX: 19.4, x1: 6, zeroGoals: 18, sixGoals: 17.1, ht11: 6.8, O45: 5.8, htgg: 4.3, ft11: 8.5, wingg: 3.8, fourGoals: 5.3 },
+  { id: "wolliv", home: "wol", away: "liv", winner: 262, fiveZero: 78.7, fiveOne: 68.8, oneX: 19.4, twoX: 19.4, x1: 6, zeroGoals: 18, sixGoals: 17.1, ht11: 6.8, O45: 5.8, htgg: 4.3, ft11: 8.5, wingg: 3.8, fourGoals: 5.3 },
+  { id: "leeliv", home: "lee", away: "liv", winner: 262, fiveZero: 78.7, fiveOne: 68.8, oneX: 19.4, twoX: 19.4, x1: 6, zeroGoals: 18, sixGoals: 17.1, ht11: 6.8, O45: 5.8, htgg: 4.3, ft11: 8.5, wingg: 3.8, fourGoals: 5.3 },
+  { id: "eveliv", home: "eve", away: "liv", winner: 262, fiveZero: 78.7, fiveOne: 68.8, oneX: 19.4, twoX: 19.4, x1: 6, zeroGoals: 18, sixGoals: 17.1, ht11: 6.8, O45: 5.8, htgg: 4.3, ft11: 8.5, wingg: 3.8, fourGoals: 5.3 },
+  { id: "burliv", home: "bur", away: "liv", winner: 168, fiveZero: 54.6, fiveOne: 54.6, oneX: 22.3, twoX: 22.3, x1: 6, zeroGoals: 15.3, sixGoals: 17.4, ht11: 6.8, O45: 5.8, htgg: 4.3, ft11: 8.5, wingg: 3.8, fourGoals: 5.3 },
+  { id: "sunliv", home: "sun", away: "liv", winner: 168, fiveZero: 54.6, fiveOne: 54.6, oneX: 22.3, twoX: 22.3, x1: 6, zeroGoals: 15.3, sixGoals: 17.4, ht11: 6.8, O45: 5.8, htgg: 4.3, ft11: 8.5, wingg: 3.8, fourGoals: 5.3 }
 ];
