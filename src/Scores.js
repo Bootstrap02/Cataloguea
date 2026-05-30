ec657d9b3b1d6aaa59d245d9de8ab674a531a779
Louis Ogochukwu Udegbue <143167253+Bootstrap02@users.noreply.github.com>
2026-05-30 07:42:29 +0100

    Update Scores.js

--- src/Scores.js
+++ src/Scores.js
@@ -19,27 +19,27 @@ export const odds = [
 ];
 
 
-
 export const smallOdds = [
-  { id: "livcry", home: "liv", away: "cry", winner: 105, fiveZero: 40.4, fiveOne: 45.5, zeroGoals: 14.7, oneGoal: 6.65, twoGoals: 4.17, threeGoals: 3.99, fourGoals: 5, fiveGoals: 8.14, sixGoals: 15.5 },
-  { id: "livbou", home: "liv", away: "bou", winner: 105, fiveZero: 40.4, fiveOne: 45.5, zeroGoals: 14.7, oneGoal: 6.65, twoGoals: 4.17, threeGoals: 3.99, fourGoals: 5, fiveGoals: 8.14, sixGoals: 15.5 },
-  { id: "livful", home: "liv", away: "ful", winner: 105, fiveZero: 40.4, fiveOne: 45.5, zeroGoals: 14.7, oneGoal: 6.65, twoGoals: 4.17, threeGoals: 3.99, fourGoals: 5, fiveGoals: 8.14, sixGoals: 15.5 },
-  { id: "livwhu", home: "liv", away: "whu", winner: 66.1, fiveZero: 28, fiveOne: 33.9, zeroGoals: 17.7, oneGoal: 7.29, twoGoals: 4.5, threeGoals: 3.94, fourGoals: 4.78, fiveGoals: 7.14, sixGoals: 13 },
-  { id: "livbre", home: "liv", away: "bre", winner: 66.1, fiveZero: 28, fiveOne: 33.9, zeroGoals: 17.7, oneGoal: 7.29, twoGoals: 4.5, threeGoals: 3.94, fourGoals: 4.78, fiveGoals: 7.14, sixGoals: 13 },
-  { id: "livwol", home: "liv", away: "wol", winner: 66.1, fiveZero: 28, fiveOne: 33.9, zeroGoals: 17.7, oneGoal: 7.29, twoGoals: 4.5, threeGoals: 3.94, fourGoals: 4.78, fiveGoals: 7.14, sixGoals: 13 },
-  { id: "livlee", home: "liv", away: "lee", winner: 66.1, fiveZero: 28, fiveOne: 33.9, zeroGoals: 17.7, oneGoal: 7.29, twoGoals: 4.5, threeGoals: 3.94, fourGoals: 4.78, fiveGoals: 7.14, sixGoals: 13 },
-  { id: "liveve", home: "liv", away: "eve", winner: 66.1, fiveZero: 28, fiveOne: 33.9, zeroGoals: 17.7, oneGoal: 7.29, twoGoals: 4.5, threeGoals: 3.94, fourGoals: 4.78, fiveGoals: 7.14, sixGoals: 13 },
-  { id: "livsun", home: "liv", away: "sun", winner: 46.3, fiveZero: 20.6, fiveOne: 31.9, zeroGoals: 16.6, oneGoal: 7.3, twoGoals: 4.43, threeGoals: 3.98, fourGoals: 4.84, fiveGoals: 7.16, sixGoals: 13.3 },
-  { id: "livbur", home: "liv", away: "bur", winner: 46.3, fiveZero: 20.6, fiveOne: 31.9, zeroGoals: 16.6, oneGoal: 7.3, twoGoals: 4.43, threeGoals: 3.98, fourGoals: 4.84, fiveGoals: 7.16, sixGoals: 13.3 },
-  { id: "livast", home: "liv", away: "ast", winner: 229, fiveZero: 71.2, fiveOne: 67.3, zeroGoals: 16, oneGoal: 6.06, twoGoals: 3.97, threeGoals: 4, fourGoals: 5.08, fiveGoals: 8.9, sixGoals: 17.2 },
-  { id: "livtot", home: "liv", away: "tot", winner: 229, fiveZero: 71.2, fiveOne: 67.3, zeroGoals: 16, oneGoal: 6.06, twoGoals: 3.97, threeGoals: 4, fourGoals: 5.08, fiveGoals: 8.9, sixGoals: 17.2 },
-  { id: "livbha", home: "liv", away: "bha", winner: 144, fiveZero: 52, fiveOne: 51.3, zeroGoals: 16, oneGoal: 6.63, twoGoals: 4.21, threeGoals: 3.97, fourGoals: 4.93, fiveGoals: 8.01, sixGoals: 15.1 },
-  { id: "livnot", home: "liv", away: "not", winner: 144, fiveZero: 52, fiveOne: 51.3, zeroGoals: 16, oneGoal: 6.63, twoGoals: 4.21, threeGoals: 3.97, fourGoals: 4.93, fiveGoals: 8.01, sixGoals: 15.1 },
-  { id: "whuliv", home: "whu", away: "liv", winner: 262, fiveZero: 78.7, fiveOne: 68.8, zeroGoals: 18, oneGoal: 5.86, twoGoals: 4.21, threeGoals: 3.83, fourGoals: 5.17, fiveGoals: 8.34, sixGoals: 17.1 },
-  { id: "breliv", home: "bre", away: "liv", winner: 262, fiveZero: 78.7, fiveOne: 68.8, zeroGoals: 18, oneGoal: 5.86, twoGoals: 4.21, threeGoals: 3.83, fourGoals: 5.17, fiveGoals: 8.34, sixGoals: 17.1 },
-  { id: "wolliv", home: "wol", away: "liv", winner: 262, fiveZero: 78.7, fiveOne: 68.8, zeroGoals: 18, oneGoal: 5.86, twoGoals: 4.21, threeGoals: 3.83, fourGoals: 5.17, fiveGoals: 8.34, sixGoals: 17.1 },
-  { id: "leeliv", home: "lee", away: "liv", winner: 262, fiveZero: 78.7, fiveOne: 68.8, zeroGoals: 18, oneGoal: 5.86, twoGoals: 4.21, threeGoals: 3.83, fourGoals: 5.17, fiveGoals: 8.34, sixGoals: 17.1 },
-  { id: "eveliv", home: "eve", away: "liv", winner: 262, fiveZero: 78.7, fiveOne: 68.8, zeroGoals: 18, oneGoal: 5.86, twoGoals: 4.21, threeGoals: 3.83, fourGoals: 5.17, fiveGoals: 8.34, sixGoals: 17.1 },
-  { id: "burliv", home: "bur", away: "liv", winner: 168, fiveZero: 54.6, fiveOne: 54.6, zeroGoals: 15.3, oneGoal: 5.96, twoGoals: 4.19, threeGoals: 3.86, fourGoals: 5.27, fiveGoals: 8.46, sixGoals: 17.4 },
-  { id: "sunliv", home: "sun", away: "liv", winner: 168, fiveZero: 54.6, fiveOne: 54.6, zeroGoals: 15.3, oneGoal: 5.96, twoGoals: 4.19, threeGoals: 3.86, fourGoals: 5.27, fiveGoals: 8.46, sixGoals: 17.4 }
+  { id: "livcry", home: "liv", away: "cry", winner: 105, win: 1.42, draw: 4.56, lose: 7.96, fiveZero: 40.4, fiveOne: 45.5, zeroGoals: 14.7, oneGoal: 6.65, twoGoals: 4.17, threeGoals: 3.99, fourGoals: 5, fiveGoals: 8.14, sixGoals: 15.5 },
+  { id: "livbou", home: "liv", away: "bou", winner: 105, win: 1.42, draw: 4.56, lose: 7.96, fiveZero: 40.4, fiveOne: 45.5, zeroGoals: 14.7, oneGoal: 6.65, twoGoals: 4.17, threeGoals: 3.99, fourGoals: 5, fiveGoals: 8.14, sixGoals: 15.5 },
+  { id: "livful", home: "liv", away: "ful", winner: 105, win: 1.42, draw: 4.56, lose: 7.96, fiveZero: 40.4, fiveOne: 45.5, zeroGoals: 14.7, oneGoal: 6.65, twoGoals: 4.17, threeGoals: 3.99, fourGoals: 5, fiveGoals: 8.14, sixGoals: 15.5 },
+  { id: "livwhu", home: "liv", away: "whu", winner: 66.1, win: 1.3, draw: 5.54, lose: 10.1, fiveZero: 28, fiveOne: 33.9, zeroGoals: 17.7, oneGoal: 7.29, twoGoals: 4.5, threeGoals: 3.94, fourGoals: 4.78, fiveGoals: 7.14, sixGoals: 13 },
+  { id: "livbre", home: "liv", away: "bre", winner: 66.1, win: 1.3, draw: 5.54, lose: 10.1, fiveZero: 28, fiveOne: 33.9, zeroGoals: 17.7, oneGoal: 7.29, twoGoals: 4.5, threeGoals: 3.94, fourGoals: 4.78, fiveGoals: 7.14, sixGoals: 13 },
+  { id: "livwol", home: "liv", away: "wol", winner: 66.1, win: 1.3, draw: 5.54, lose: 10.1, fiveZero: 28, fiveOne: 33.9, zeroGoals: 17.7, oneGoal: 7.29, twoGoals: 4.5, threeGoals: 3.94, fourGoals: 4.78, fiveGoals: 7.14, sixGoals: 13 },
+  { id: "livlee", home: "liv", away: "lee", winner: 66.1, win: 1.3, draw: 5.54, lose: 10.1, fiveZero: 28, fiveOne: 33.9, zeroGoals: 17.7, oneGoal: 7.29, twoGoals: 4.5, threeGoals: 3.94, fourGoals: 4.78, fiveGoals: 7.14, sixGoals: 13 },
+  { id: "liveve", home: "liv", away: "eve", winner: 66.1, win: 1.3, draw: 5.54, lose: 10.1, fiveZero: 28, fiveOne: 33.9, zeroGoals: 17.7, oneGoal: 7.29, twoGoals: 4.5, threeGoals: 3.94, fourGoals: 4.78, fiveGoals: 7.14, sixGoals: 13 },
+  { id: "livsun", home: "liv", away: "sun", winner: 46.3, win: 1.23, draw: 6.23, lose: 13.1, fiveZero: 20.6, fiveOne: 31.9, zeroGoals: 16.6, oneGoal: 7.3, twoGoals: 4.43, threeGoals: 3.98, fourGoals: 4.84, fiveGoals: 7.16, sixGoals: 13.3 },
+  { id: "livbur", home: "liv", away: "bur", winner: 46.3, win: 1.23, draw: 6.23, lose: 13.1, fiveZero: 20.6, fiveOne: 31.9, zeroGoals: 16.6, oneGoal: 7.3, twoGoals: 4.43, threeGoals: 3.98, fourGoals: 4.84, fiveGoals: 7.16, sixGoals: 13.3 },
+  { id: "livast", home: "liv", away: "ast", winner: 229, win: 1.57, draw: 4.04, lose: 6.04, fiveZero: 71.2, fiveOne: 67.3, zeroGoals: 16, oneGoal: 6.06, twoGoals: 3.97, threeGoals: 4, fourGoals: 5.08, fiveGoals: 8.9, sixGoals: 17.2 },
+  { id: "livtot", home: "liv", away: "tot", winner: 229, win: 1.57, draw: 4.04, lose: 6.04, fiveZero: 71.2, fiveOne: 67.3, zeroGoals: 16, oneGoal: 6.06, twoGoals: 3.97, threeGoals: 4, fourGoals: 5.08, fiveGoals: 8.9, sixGoals: 17.2 },
+  { id: "livbha", home: "liv", away: "bha", winner: 144, win: 1.47, draw: 4.39, lose: 7.04, fiveZero: 52, fiveOne: 51.3, zeroGoals: 16, oneGoal: 6.63, twoGoals: 4.21, threeGoals: 3.97, fourGoals: 4.93, fiveGoals: 8.01, sixGoals: 15.1 },
+  { id: "livnot", home: "liv", away: "not", winner: 144, win: 1.47, draw: 4.39, lose: 7.04, fiveZero: 52, fiveOne: 51.3, zeroGoals: 16, oneGoal: 6.63, twoGoals: 4.21, threeGoals: 3.97, fourGoals: 4.93, fiveGoals: 8.01, sixGoals: 15.1 },
+  { id: "whuliv", home: "whu", away: "liv", winner: 262, win: 7.63, draw: 4.67, lose: 1.42, fiveZero: 78.7, fiveOne: 68.8, zeroGoals: 18, oneGoal: 5.86, twoGoals: 4.21, threeGoals: 3.83, fourGoals: 5.17, fiveGoals: 8.34, sixGoals: 17.1 },
+  { id: "breliv", home: "bre", away: "liv", winner: 262, win: 7.63, draw: 4.67, lose: 1.42, fiveZero: 78.7, fiveOne: 68.8, zeroGoals: 18, oneGoal: 5.86, twoGoals: 4.21, threeGoals: 3.83, fourGoals: 5.17, fiveGoals: 8.34, sixGoals: 17.1 },
+  { id: "wolliv", home: "wol", away: "liv", winner: 262, win: 7.63, draw: 4.67, lose: 1.42, fiveZero: 78.7, fiveOne: 68.8, zeroGoals: 18, oneGoal: 5.86, twoGoals: 4.21, threeGoals: 3.83, fourGoals: 5.17, fiveGoals: 8.34, sixGoals: 17.1 },
+  { id: "leeliv", home: "lee", away: "liv", winner: 262, win: 7.63, draw: 4.67, lose: 1.42, fiveZero: 78.7, fiveOne: 68.8, zeroGoals: 18, oneGoal: 5.86, twoGoals: 4.21, threeGoals: 3.83, fourGoals: 5.17, fiveGoals: 8.34, sixGoals: 17.1 },
+  { id: "eveliv", home: "eve", away: "liv", winner: 262, win: 7.63, draw: 4.67, lose: 1.42, fiveZero: 78.7, fiveOne: 68.8, zeroGoals: 18, oneGoal: 5.86, twoGoals: 4.21, threeGoals: 3.83, fourGoals: 5.17, fiveGoals: 8.34, sixGoals: 17.1 },
+  { id: "burliv", home: "bur", away: "liv", winner: 168, win: 8.64, draw: 4.91, lose: 1.37, fiveZero: 54.6, fiveOne: 54.6, zeroGoals: 15.3, oneGoal: 5.96, twoGoals: 4.19, threeGoals: 3.86, fourGoals: 5.27, fiveGoals: 8.46, sixGoals: 17.4 },
+  { id: "sunliv", home: "sun", away: "liv", winner: 168, win: 8.64, draw: 4.91, lose: 1.37, fiveZero: 54.6, fiveOne: 54.6, zeroGoals: 15.3, oneGoal: 5.96, twoGoals: 4.19, threeGoals: 3.86, fourGoals: 5.27, fiveGoals: 8.46, sixGoals: 17.4 }
 ];
+
