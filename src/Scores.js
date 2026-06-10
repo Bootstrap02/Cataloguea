a1802d6094f7099c6d796e3bf843b2b972785cac
Bootstrap02 <louisjoseph131@gmail.com>
2026-06-10 11:31:10 +0100

    changes to the liverool odds

--- src/Scores.js
+++ src/Scores.js
@@ -1,68 +1,67 @@
 
 export const odds = [
-  // winner: 327 - Liverpool home vs weaker teams
-  { id: "livmnu", home: "liv", away: "mnu", winner: 327, oneX: 14.8, twoX: 14.8, x2: 14.7, tg0: 15, tg6: 17.3, ht12: 22.4, ht21: 13.4, ht30: 26.2, ft40: 36, ft41: 30.3 },
-  { id: "livnew", home: "liv", away: "new", winner: 327, oneX: 14.8, twoX: 14.8, x2: 14.7, tg0: 15, tg6: 17.3, ht12: 22.4, ht21: 13.4, ht30: 26.2, ft40: 36, ft41: 30.3 },
-  { id: "livche", home: "liv", away: "che", winner: 327, oneX: 14.8, twoX: 14.8, x2: 14.7, tg0: 15, tg6: 17.3, ht12: 22.4, ht21: 13.4, ht30: 26.2, ft40: 36, ft41: 30.3 },
+  //DONE........... winner: 327 - Liverpool home vs weaker teams
+  { id: "livmnu", home: "liv", away: "mnu", winner: 325, oneX: 14.7, twoX: 14.7, x2: 14.6, tg0: 15.1, tg6: 16.9, ht12: 22.4, ht21: 13.4, ht30: 26.2, ft40: 35.8, ft41: 30 },
+  { id: "livnew", home: "liv", away: "new", winner: 325, oneX: 14.7, twoX: 14.7, x2: 14.6, tg0: 15.1, tg6: 16.9, ht12: 22.4, ht21: 13.4, ht30: 26.2, ft40: 35.8, ft41: 30 },
+  { id: "livche", home: "liv", away: "che", winner: 325, oneX: 14.7, twoX: 14.7, x2: 14.6, tg0: 15.1, tg6: 16.9, ht12: 22.4, ht21: 13.4, ht30: 26.2, ft40: 35.8, ft41: 30 },
 
-  // winner: 416 - Liverpool away vs Bournemouth/Fulham/Crystal Palace
-  { id: "bouliv", home: "bou", away: "liv", winner: 416, oneX: 18.4, twoX: 18.4, x2: 19.1, tg0: 16.7, tg6: 20.1, ht12: 27.2, ht21: 13.5, ht30: 26.8, ft40: 36.6, ft41: 30.1 },
-  { id: "fulliv", home: "ful", away: "liv", winner: 416, oneX: 18.4, twoX: 18.4, x2: 19.1, tg0: 16.7, tg6: 20.1, ht12: 27.2, ht21: 13.5, ht30: 26.8, ft40: 36.6, ft41: 30.1 },
-  { id: "cryliv", home: "cry", away: "liv", winner: 416, oneX: 18.4, twoX: 18.4, x2: 19.1, tg0: 16.7, tg6: 20.1, ht12: 27.2, ht21: 13.5, ht30: 26.8, ft40: 36.6, ft41: 30.1 },
+  //DONE..... winner: 416 - Liverpool away vs Bournemouth/Fulham/Crystal Palace
+  { id: "bouliv", home: "bou", away: "liv", winner: 413, oneX: 18.3, twoX: 18.3, x2: 19, tg0: 16.8, tg6: 19.6, ht12: 27.2, ht21: 13.5, ht30: 26.8, ft40: 36.3, ft41: 29.8 },
+  { id: "fulliv", home: "ful", away: "liv", winner: 413, oneX: 18.3, twoX: 18.3, x2: 19, tg0: 16.8, tg6: 19.6, ht12: 27.2, ht21: 13.5, ht30: 26.8, ft40: 36.3, ft41: 29.8 },
+  { id: "cryliv", home: "cry", away: "liv", winner: 413, oneX: 18.3, twoX: 18.3, x2: 19, tg0: 16.8, tg6: 19.6, ht12: 27.2, ht21: 13.5, ht30: 26.8, ft40: 36.3, ft41: 29.8 },
 
-  // winner: 847 - Liverpool away vs Aston Villa/Tottenham
-  { id: "astliv", home: "ast", away: "liv", winner: 847, oneX: 16.8, twoX: 16.8, x2: 13.3, tg0: 13, tg6: 23.7, ht12: 24.7, ht21: 16.2, ht30: 40.2, ft40: 58.6, ft41: 44.3 },
-  { id: "totliv", home: "tot", away: "liv", winner: 847, oneX: 16.8, twoX: 16.8, x2: 13.3, tg0: 13, tg6: 23.7, ht12: 24.7, ht21: 16.2, ht30: 40.2, ft40: 58.6, ft41: 44.3 },
+  //DONE....... winner: 847 - Liverpool away vs Aston Villa/Tottenham
+  { id: "astliv", home: "ast", away: "liv", winner: 841, oneX: 16.7, twoX: 16.7, x2: 13.3, tg0: 13.1, tg6: 23.1, ht12: 24.7, ht21: 16.2, ht30: 40.1, ft40: 58.2, ft41: 43.9 },
+  { id: "totliv", home: "tot", away: "liv", winner: 841, oneX: 16.7, twoX: 16.7, x2: 13.3, tg0: 13.1, tg6: 23.1, ht12: 24.7, ht21: 16.2, ht30: 40.1, ft40: 58.2, ft41: 43.9 },
 
-  // winner: 229 - Liverpool home vs Wolves/Aston Villa/Tottenham
-  { id: "livast", home: "liv", away: "ast", winner: 229, oneX: 16.6, twoX: 16.6, x2: 18.2, tg0: 16, tg6: 17.2, ht12: 26.3, ht21: 13, ht30: 21.4, ft40: 27.7, ft41: 26.1 },
-  { id: "livtot", home: "liv", away: "tot", winner: 229, oneX: 16.6, twoX: 16.6, x2: 18.2, tg0: 16, tg6: 17.2, ht12: 26.3, ht21: 13, ht30: 21.4, ft40: 27.7, ft41: 26.1 },
+  //DONE ........ winner: 229 - Liverpool home vs Wolves/Aston Villa/Tottenham
+  { id: "livast", home: "liv", away: "ast", winner: 228, oneX: 16.6, twoX: 16.6, x2: 18.2, tg0: 16.1, tg6: 16.8, ht12: 26.2, ht21: 13, ht30: 21.4, ft40: 27.5, ft41: 25.9 },
+  { id: "livtot", home: "liv", away: "tot", winner: 228, oneX: 16.6, twoX: 16.6, x2: 18.2, tg0: 16.1, tg6: 16.8, ht12: 26.2, ht21: 13, ht30: 21.4, ft40: 27.5, ft41: 25.9 },
 
-  // winner: 560 - Liverpool away vs Nottingham/Brighton
-  { id: "notliv", home: "not", away: "liv", winner: 560, oneX: 16.4, twoX: 16.4, x2: 16, tg0: 14.8, tg6: 21.2, ht12: 25.3, ht21: 14.6, ht30: 32.3, ft40: 44.8, ft41: 36.3 },
-  { id: "bhaliv", home: "bha", away: "liv", winner: 560, oneX: 16.4, twoX: 16.4, x2: 16, tg0: 14.8, tg6: 21.2, ht12: 25.3, ht21: 14.6, ht30: 32.3, ft40: 44.8, ft41: 36.3 },
+  //DONE........ winner: 560 - Liverpool away vs Nottingham/Brighton
+  { id: "notliv", home: "not", away: "liv", winner: 556, oneX: 16.4, twoX: 16.4, x2: 16, tg0: 14.9, tg6: 20.7, ht12: 25.3, ht21: 14.6, ht30: 32.3, ft40: 44.5, ft41: 36 },
+  { id: "bhaliv", home: "bha", away: "liv", winner: 556, oneX: 16.4, twoX: 16.4, x2: 16, tg0: 14.9, tg6: 20.7, ht12: 25.3, ht21: 14.6, ht30: 32.3, ft40: 44.5, ft41: 36 },
 
-  // winner: 262 - Liverpool away vs West Ham/Brentford/Wolves/Leeds/Everton
-  { id: "whuliv", home: "whu", away: "liv", winner: 262, oneX: 19.4, twoX: 19.4, x2: 23.7, tg0: 18, tg6: 17.1, ht12: 27.7, ht21: 12, ht30: 21.2, ft40: 28.8, ft41: 24.2 },
-  { id: "breliv", home: "bre", away: "liv", winner: 262, oneX: 19.4, twoX: 19.4, x2: 23.7, tg0: 18, tg6: 17.1, ht12: 27.7, ht21: 12, ht30: 21.2, ft40: 28.8, ft41: 24.2 },
-  { id: "wolliv", home: "wol", away: "liv", winner: 262, oneX: 19.4, twoX: 19.4, x2: 23.7, tg0: 18, tg6: 17.1, ht12: 27.7, ht21: 12, ht30: 21.2, ft40: 28.8, ft41: 24.2 },
-  { id: "leeliv", home: "lee", away: "liv", winner: 262, oneX: 19.4, twoX: 19.4, x2: 23.7, tg0: 18, tg6: 17.1, ht12: 27.7, ht21: 12, ht30: 21.2, ft40: 28.8, ft41: 24.2 },
-  { id: "eveliv", home: "eve", away: "liv", winner: 262, oneX: 19.4, twoX: 19.4, x2: 23.7, tg0: 18, tg6: 17.1, ht12: 27.7, ht21: 12, ht30: 21.2, ft40: 28.8, ft41: 24.2 },
+  //DONE............... winner: 262 - Liverpool away vs West Ham/Brentford/Wolves/Leeds/Everton
+  { id: "whuliv", home: "whu", away: "liv", winner: 260, oneX: 19.3, twoX: 19.3, x2: 23.6, tg0: 18.2, tg6: 16.7, ht12: 27.6, ht21: 12, ht30: 21.1, ft40: 28.6, ft41: 24 },
+  { id: "breliv", home: "bre", away: "liv", winner: 260, oneX: 19.3, twoX: 19.3, x2: 23.6, tg0: 18.2, tg6: 16.7, ht12: 27.6, ht21: 12, ht30: 21.1, ft40: 28.6, ft41: 24 },
+  { id: "wolliv", home: "wol", away: "liv", winner: 260, oneX: 19.3, twoX: 19.3, x2: 23.6, tg0: 18.2, tg6: 16.7, ht12: 27.6, ht21: 12, ht30: 21.1, ft40: 28.6, ft41: 24 },
+  { id: "leeliv", home: "lee", away: "liv", winner: 260, oneX: 19.3, twoX: 19.3, x2: 23.6, tg0: 18.2, tg6: 16.7, ht12: 27.6, ht21: 12, ht30: 21.1, ft40: 28.6, ft41: 24 },
+  { id: "eveliv", home: "eve", away: "liv", winner: 260, oneX: 19.3, twoX: 19.3, x2: 23.6, tg0: 18.2, tg6: 16.7, ht12: 27.6, ht21: 12, ht30: 21.1, ft40: 28.6, ft41: 24 },
 
-  // winner: 1226 - Liverpool away vs Man City/Arsenal
-  { id: "mncliv", home: "mnc", away: "liv", winner: 1226, oneX: 14.8, twoX: 14.8, x2: 9.55, tg0: 13.8, tg6: 20.5, ht12: 18.5, ht21: 17, ht30: 50.8, ft40: 79.3, ft41: 57.7 },
-  { id: "arsliv", home: "ars", away: "liv", winner: 1226, oneX: 14.8, twoX: 14.8, x2: 9.55, tg0: 13.8, tg6: 20.5, ht12: 18.5, ht21: 17, ht30: 50.8, ft40: 79.3, ft41: 57.7 },
+  //DONE..... winner: 1226 - Liverpool away vs Man City/Arsenal
+  { id: "mncliv", home: "mnc", away: "liv", winner: 1217, oneX: 14.7, twoX: 14.7, x2: 9.52, tg0: 13.9, tg6: 20, ht12: 18.5, ht21: 16.9, ht30: 50.8, ft40: 78.7, ft41: 57.2 },
+  { id: "arsliv", home: "ars", away: "liv", winner: 1217, oneX: 14.7, twoX: 14.7, x2: 9.52, tg0: 13.9, tg6: 20, ht12: 18.5, ht21: 16.9, ht30: 50.8, ft40: 78.7, ft41: 57.2 },
 
-  // winner: 144 - Liverpool home vs Brighton/Nottingham
-  { id: "livbha", home: "liv", away: "bha", winner: 144, oneX: 18.6, twoX: 18.6, x2: 21.1, tg0: 16, tg6: 15.1, ht12: 27.6, ht21: 12, ht30: 17, ft40: 22, ft41: 21.6 },
-  { id: "livnot", home: "liv", away: "not", winner: 144, oneX: 18.6, twoX: 18.6, x2: 21.1, tg0: 16, tg6: 15.1, ht12: 27.6, ht21: 12, ht30: 17, ft40: 22, ft41: 21.6 },
+  //DONE........... winner: 144 - Liverpool home vs Brighton/Nottingham
+  { id: "livbha", home: "liv", away: "bha", winner: 143, oneX: 18.5, twoX: 18.5, x2: 21.1, tg0: 16.1, tg6: 14.7, ht12: 27.5, ht21: 11.9, ht30: 17, ft40: 21.8, ft41: 21.4 },
+  { id: "livnot", home: "liv", away: "not", winner: 143, oneX: 18.5, twoX: 18.5, x2: 21.1, tg0: 16.1, tg6: 14.7, ht12: 27.5, ht21: 11.9, ht30: 17, ft40: 21.8, ft41: 21.4 },
 
-  // winner: 568 - Liverpool home vs Man City/Arsenal
-  { id: "livmnc", home: "liv", away: "mnc", winner: 568, oneX: 13.8, twoX: 13.8, x2: 11.9, tg0: 11.1, tg6: 19, ht12: 20.5, ht21: 15.2, ht30: 36.2, ft40: 52.4, ft41: 41.1 },
-  { id: "livars", home: "liv", away: "ars", winner: 568, oneX: 13.8, twoX: 13.8, x2: 11.9, tg0: 11.1, tg6: 19, ht12: 20.5, ht21: 15.2, ht30: 36.2, ft40: 52.4, ft41: 41.1 },
+  //DONE........... winner: 568 - Liverpool home vs Man City/Arsenal
+  { id: "livmnc", home: "liv", away: "mnc", winner: 563, oneX: 13.7, twoX: 13.7, x2: 11.9, tg0: 11.2, tg6: 18.6, ht12: 20.5, ht21: 15.1, ht30: 36.2, ft40: 52, ft41: 40.8 },
+  { id: "livars", home: "liv", away: "ars", winner: 563, oneX: 13.7, twoX: 13.7, x2: 11.9, tg0: 11.2, tg6: 18.6, ht12: 20.5, ht21: 15.1, ht30: 36.2, ft40: 52, ft41: 40.8 },
+  //DONE....... winner: 1140 - Liverpool away vs Man United/Newcastle/Chelsea
+  { id: "mnuliv", home: "mnu", away: "liv", winner: 1131, oneX: 15.7, twoX: 15.7, x2: 11.6, tg0: 13, tg6: 21.6, ht12: 21.4, ht21: 16.4, ht30: 45.9, ft40: 72.2, ft41: 50.2 },
+  { id: "newliv", home: "new", away: "liv", winner: 1131, oneX: 15.7, twoX: 15.7, x2: 11.6, tg0: 13, tg6: 21.6, ht12: 21.4, ht21: 16.4, ht30: 45.9, ft40: 72.2, ft41: 50.2 },
+  { id: "cheliv", home: "che", away: "liv", winner: 1131, oneX: 15.7, twoX: 15.7, x2: 11.6, tg0: 13, tg6: 21.6, ht12: 21.4, ht21: 16.4, ht30: 45.9, ft40: 72.2, ft41: 50.2 },
 
-  // winner: 1140 - Liverpool away vs Man United/Newcastle/Chelsea
-  { id: "mnuliv", home: "mnu", away: "liv", winner: 1140, oneX: 15.7, twoX: 15.7, x2: 11.6, tg0: 12.9, tg6: 22, ht12: 21.5, ht21: 16.4, ht30: 46, ft40: 72.8, ft41: 50.6 },
-  { id: "newliv", home: "new", away: "liv", winner: 1140, oneX: 15.7, twoX: 15.7, x2: 11.6, tg0: 12.9, tg6: 22, ht12: 21.5, ht21: 16.4, ht30: 46, ft40: 72.8, ft41: 50.6 },
-  { id: "cheliv", home: "che", away: "liv", winner: 1140, oneX: 15.7, twoX: 15.7, x2: 11.6, tg0: 12.9, tg6: 22, ht12: 21.5, ht21: 16.4, ht30: 46, ft40: 72.8, ft41: 50.6 },
+  //DONE....... winner: 105 - Liverpool home vs Crystal Palace/Bournemouth/Fulham
+  { id: "livcry", home: "liv", away: "cry", winner: 104, oneX: 20.4, twoX: 20.4, x2: 22.9, tg0: 14.8, tg6: 15.1, ht12: 33, ht21: 12.6, ht30: 14.9, ft40: 17.9, ft41: 20.4 },
+  { id: "livbou", home: "liv", away: "bou", winner: 104, oneX: 20.4, twoX: 20.4, x2: 22.9, tg0: 14.8, tg6: 15.1, ht12: 33, ht21: 12.6, ht30: 14.9, ft40: 17.9, ft41: 20.4 },
+  { id: "livful", home: "liv", away: "ful", winner: 104, oneX: 20.4, twoX: 20.4, x2: 22.9, tg0: 14.8, tg6: 15.1, ht12: 33, ht21: 12.6, ht30: 14.9, ft40: 17.9, ft41: 20.4 },
 
-  // winner: 105 - Liverpool home vs Crystal Palace/Bournemouth/Fulham
-  { id: "livcry", home: "liv", away: "cry", winner: 105, oneX: 20.4, twoX: 20.4, x2: 23, tg0: 14.7, tg6: 15.5, ht12: 33.1, ht21: 12.6, ht30: 14.9, ft40: 18, ft41: 20.6 },
-  { id: "livbou", home: "liv", away: "bou", winner: 105, oneX: 20.4, twoX: 20.4, x2: 23, tg0: 14.7, tg6: 15.5, ht12: 33.1, ht21: 12.6, ht30: 14.9, ft40: 18, ft41: 20.6 },
-  { id: "livful", home: "liv", away: "ful", winner: 105, oneX: 20.4, twoX: 20.4, x2: 23, tg0: 14.7, tg6: 15.5, ht12: 33.1, ht21: 12.6, ht30: 14.9, ft40: 18, ft41: 20.6 },
+  // dDONE...........winner: 66.1 - Liverpool home vs West Ham/Brentford/Wolves/Leeds/Everton
+  { id: "livwhu", home: "liv", away: "whu", winner: 65.6, oneX: 24.9, twoX: 24.9, x2: 28.8, tg0: 17.8, tg6: 12.7, ht12: 37.3, ht21: 11.6, ht30: 11.3, ft40: 13.9, ft41: 16.7 },
+  { id: "livbre", home: "liv", away: "bre", winner: 65.6, oneX: 24.9, twoX: 24.9, x2: 28.8, tg0: 17.8, tg6: 12.7, ht12: 37.3, ht21: 11.6, ht30: 11.3, ft40: 13.9, ft41: 16.7 },
+  { id: "livwol", home: "liv", away: "wol", winner: 65.6, oneX: 24.9, twoX: 24.9, x2: 28.8, tg0: 17.8, tg6: 12.7, ht12: 37.3, ht21: 11.6, ht30: 11.3, ft40: 13.9, ft41: 16.7 },
+  { id: "livlee", home: "liv", away: "lee", winner: 65.6, oneX: 24.9, twoX: 24.9, x2: 28.8, tg0: 17.8, tg6: 12.7, ht12: 37.3, ht21: 11.6, ht30: 11.3, ft40: 13.9, ft41: 16.7 },
+  { id: "liveve", home: "liv", away: "eve", winner: 65.6, oneX: 24.9, twoX: 24.9, x2: 28.8, tg0: 17.8, tg6: 12.7, ht12: 37.3, ht21: 11.6, ht30: 11.3, ft40: 13.9, ft41: 16.7 },
 
-  // winner: 66.1 - Liverpool home vs West Ham/Brentford/Wolves/Leeds/Everton
-  { id: "livwhu", home: "liv", away: "whu", winner: 66.1, oneX: 24.9, twoX: 24.9, x2: 28.8, tg0: 17.7, tg6: 13, ht12: 37.3, ht21: 11.6, ht30: 11.3, ft40: 14, ft41: 16.9 },
-  { id: "livbre", home: "liv", away: "bre", winner: 66.1, oneX: 24.9, twoX: 24.9, x2: 28.8, tg0: 17.7, tg6: 13, ht12: 37.3, ht21: 11.6, ht30: 11.3, ft40: 14, ft41: 16.9 },
-  { id: "livwol", home: "liv", away: "wol", winner: 66.1, oneX: 24.9, twoX: 24.9, x2: 28.8, tg0: 17.7, tg6: 13, ht12: 37.3, ht21: 11.6, ht30: 11.3, ft40: 14, ft41: 16.9 },
-  { id: "livlee", home: "liv", away: "lee", winner: 66.1, oneX: 24.9, twoX: 24.9, x2: 28.8, tg0: 17.7, tg6: 13, ht12: 37.3, ht21: 11.6, ht30: 11.3, ft40: 14, ft41: 16.9 },
-  { id: "liveve", home: "liv", away: "eve", winner: 66.1, oneX: 24.9, twoX: 24.9, x2: 28.8, tg0: 17.7, tg6: 13, ht12: 37.3, ht21: 11.6, ht30: 11.3, ft40: 14, ft41: 16.9 },
+  //DONE......... winner: 46.3 - Liverpool home vs Sunderland/Burnley
+  { id: "livsun", home: "liv", away: "sun", winner: 45.9, oneX: 31.1, twoX: 31.1, x2: 35.5, tg0: 16.7, tg6: 13, ht12: 53.4, ht21: 13.1, ht30: 9.62, ft40: 11, ft41: 16.6 },
+  { id: "livbur", home: "liv", away: "bur", winner: 45.9, oneX: 31.1, twoX: 31.1, x2: 35.5, tg0: 16.7, tg6: 13, ht12: 53.4, ht21: 13.1, ht30: 9.62, ft40: 11, ft41: 16.6 },
 
-  // winner: 46.3 - Liverpool home vs Sunderland/Burnley
-  { id: "livsun", home: "liv", away: "sun", winner: 46.3, oneX: 31.2, twoX: 31.2, x2: 35.6, tg0: 16.6, tg6: 13.3, ht12: 53.4, ht21: 13.1, ht30: 9.63, ft40: 11.1, ft41: 16.8 },
-  { id: "livbur", home: "liv", away: "bur", winner: 46.3, oneX: 31.2, twoX: 31.2, x2: 35.6, tg0: 16.6, tg6: 13.3, ht12: 53.4, ht21: 13.1, ht30: 9.63, ft40: 11.1, ft41: 16.8 },
-
-  // winner: 168 - Liverpool away vs Burnley/Sunderland
-  { id: "burliv", home: "bur", away: "liv", winner: 168, oneX: 22.3, twoX: 22.3, x2: 25.5, tg0: 15.3, tg6: 17.4, ht12: 33.8, ht21: 12.7, ht30: 17.7, ft40: 21.8, ft41: 21.8 },
-  { id: "sunliv", home: "sun", away: "liv", winner: 168, oneX: 22.3, twoX: 22.3, x2: 25.5, tg0: 15.3, tg6: 17.4, ht12: 33.8, ht21: 12.7, ht30: 17.7, ft40: 21.8, ft41: 21.8 }
+  //DONE ....... winner: 168 - Liverpool away vs Burnley/Sunderland
+  { id: "burliv", home: "bur", away: "liv", winner: 166, oneX: 22.3, twoX: 22.3, x2: 25.4, tg0: 15.5, tg6: 17, ht12: 33.8, ht21: 12.7, ht30: 17.6, ft40: 21.7, ft41: 21.7 },
+  { id: "sunliv", home: "sun", away: "liv", winner: 166, oneX: 22.3, twoX: 22.3, x2: 25.4, tg0: 15.5, tg6: 17, ht12: 33.8, ht21: 12.7, ht30: 17.6, ft40: 21.7, ft41: 21.7 },
 ];
