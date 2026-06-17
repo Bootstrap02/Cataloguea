f99a47570ba291aed9eb6f9eb8dc9ce0cf3a5596
Louis Ogochukwu Udegbue <143167253+Bootstrap02@users.noreply.github.com>
2026-06-17 20:32:07 +0100

    Update Scores.js

--- src/Scores.js
+++ src/Scores.js
@@ -14,28 +14,16 @@ export const odds = [
   { id: "astliv", home: "ast", away: "liv", winner: 847, win: 4.17, draw: 3.82, lose: 1.83, code: "HDA" },
   { id: "totliv", home: "tot", away: "liv", winner: 847, win: 4.17, draw: 3.82, lose: 1.83, code: "HDA" },
 
-  // winner: 229 - Liverpool home vs Wolves/Aston Villa/Tottenham
-  { id: "livast", home: "liv", away: "ast", winner: 229, win: 1.57, draw: 4.04, lose: 6.04, code: "ADH" },
-  { id: "livtot", home: "liv", away: "tot", winner: 229, win: 1.57, draw: 4.04, lose: 6.04, code: "ADH" },
 
   // winner: 560 - Liverpool away vs Nottingham/Brighton
   { id: "notliv", home: "not", away: "liv", winner: 560, win: 5.09, draw: 3.92, lose: 1.67, code: "HDA" },
   { id: "bhaliv", home: "bha", away: "liv", winner: 560, win: 5.09, draw: 3.92, lose: 1.67, code: "HDA" },
 
-  // winner: 262 - Liverpool away vs West Ham/Brentford/Wolves/Leeds/Everton
-  { id: "whuliv", home: "whu", away: "liv", winner: 262, win: 7.63, draw: 4.67, lose: 1.42, code: "HDA" },
-  { id: "breliv", home: "bre", away: "liv", winner: 262, win: 7.63, draw: 4.67, lose: 1.42, code: "HDA" },
-  { id: "wolliv", home: "wol", away: "liv", winner: 262, win: 7.63, draw: 4.67, lose: 1.42, code: "HDA" },
-  { id: "leeliv", home: "lee", away: "liv", winner: 262, win: 7.63, draw: 4.67, lose: 1.42, code: "HDA" },
-  { id: "eveliv", home: "eve", away: "liv", winner: 262, win: 7.63, draw: 4.67, lose: 1.42, code: "HDA" },
 
   // winner: 1226 - Liverpool away vs Man City/Arsenal
   { id: "mncliv", home: "mnc", away: "liv", winner: 1226, win: 2.87, draw: 3.56, lose: 2.38, code: "DHA" },
   { id: "arsliv", home: "ars", away: "liv", winner: 1226, win: 2.87, draw: 3.56, lose: 2.38, code: "DHA" },
 
-  // winner: 144 - Liverpool home vs Brighton/Nottingham
-  { id: "livbha", home: "liv", away: "bha", winner: 144, win: 1.47, draw: 4.39, lose: 7.04, code: "ADH" },
-  { id: "livnot", home: "liv", away: "not", winner: 144, win: 1.47, draw: 4.39, lose: 7.04, code: "ADH" },
 
   // winner: 568 - Liverpool home vs Man City/Arsenal
   { id: "livmnc", home: "liv", away: "mnc", winner: 568, win: 2.13, draw: 3.19, lose: 3.76, code: "ADH" },
@@ -47,39 +35,40 @@ export const odds = [
   { id: "cheliv", home: "che", away: "liv", winner: 1140, win: 3.53, draw: 3.66, lose: 2.03, code: "DHA" }
 ];
 
+
 export const smallOdds = [
-  // winner: 105 - Liverpool home vs Crystal Palace/Bournemouth/Fulham
-  { id: "livcry", home: "liv", away: "cry", winner: 105, oneZero: 14.7, twoZero: 4.17, twoOne: 6.65, threeZero: 3.99, threeOne: 6.5, threeTwo: 8.2, fourZero: 5.0, fourOne: 8.5, fourTwo: 12.3, fiveZero: 8.14, fiveOne: 11.2 },
-  { id: "livbou", home: "liv", away: "bou", winner: 105, oneZero: 14.7, twoZero: 4.17, twoOne: 6.65, threeZero: 3.99, threeOne: 6.5, threeTwo: 8.2, fourZero: 5.0, fourOne: 8.5, fourTwo: 12.3, fiveZero: 8.14, fiveOne: 11.2 },
-  { id: "livful", home: "liv", away: "ful", winner: 105, oneZero: 14.7, twoZero: 4.17, twoOne: 6.65, threeZero: 3.99, threeOne: 6.5, threeTwo: 8.2, fourZero: 5.0, fourOne: 8.5, fourTwo: 12.3, fiveZero: 8.14, fiveOne: 11.2 },
+  // winner: 104 - Liverpool home vs Crystal Palace/Bournemouth/Fulham
+  { id: "livcry", home: "liv", away: "cry", winner: 104, oneZero: 8.50, twoZero: 7.80, twoOne: 8.90, threeZero: 9.50, threeOne: 11.80, threeTwo: 25.50, fourZero: 15.20, fourOne: 18.40, fourTwo: 44.60, fiveZero: 38.70, fiveOne: 42.30, sixZero: 104.00 },
+  { id: "livbou", home: "liv", away: "bou", winner: 104, oneZero: 8.50, twoZero: 7.80, twoOne: 8.90, threeZero: 9.50, threeOne: 11.80, threeTwo: 25.50, fourZero: 15.20, fourOne: 18.40, fourTwo: 44.60, fiveZero: 38.70, fiveOne: 42.30, sixZero: 104.00 },
+  { id: "livful", home: "liv", away: "ful", winner: 104, oneZero: 8.50, twoZero: 7.80, twoOne: 8.90, threeZero: 9.50, threeOne: 11.80, threeTwo: 25.50, fourZero: 15.20, fourOne: 18.40, fourTwo: 44.60, fiveZero: 38.70, fiveOne: 42.30, sixZero: 104.00 },
 
-  // winner: 66.1 - Liverpool home vs West Ham/Brentford/Wolves/Leeds/Everton
-  { id: "livwhu", home: "liv", away: "whu", winner: 66.1, oneZero: 17.7, twoZero: 4.5, twoOne: 7.29, threeZero: 3.94, threeOne: 6.2, threeTwo: 7.9, fourZero: 4.78, fourOne: 8.1, fourTwo: 11.8, fiveZero: 7.14, fiveOne: 10.5 },
-  { id: "livbre", home: "liv", away: "bre", winner: 66.1, oneZero: 17.7, twoZero: 4.5, twoOne: 7.29, threeZero: 3.94, threeOne: 6.2, threeTwo: 7.9, fourZero: 4.78, fourOne: 8.1, fourTwo: 11.8, fiveZero: 7.14, fiveOne: 10.5 },
-  { id: "livwol", home: "liv", away: "wol", winner: 66.1, oneZero: 17.7, twoZero: 4.5, twoOne: 7.29, threeZero: 3.94, threeOne: 6.2, threeTwo: 7.9, fourZero: 4.78, fourOne: 8.1, fourTwo: 11.8, fiveZero: 7.14, fiveOne: 10.5 },
-  { id: "livlee", home: "liv", away: "lee", winner: 66.1, oneZero: 17.7, twoZero: 4.5, twoOne: 7.29, threeZero: 3.94, threeOne: 6.2, threeTwo: 7.9, fourZero: 4.78, fourOne: 8.1, fourTwo: 11.8, fiveZero: 7.14, fiveOne: 10.5 },
-  { id: "liveve", home: "liv", away: "eve", winner: 66.1, oneZero: 17.7, twoZero: 4.5, twoOne: 7.29, threeZero: 3.94, threeOne: 6.2, threeTwo: 7.9, fourZero: 4.78, fourOne: 8.1, fourTwo: 11.8, fiveZero: 7.14, fiveOne: 10.5 },
+  // winner: 66 - Liverpool home vs West Ham/Brentford/Wolves/Leeds/Everton
+  { id: "livwhu", home: "liv", away: "whu", winner: 66, oneZero: 8.55, twoZero: 8.15, twoOne: 7.96, threeZero: 11.50, threeOne: 11.30, threeTwo: 22.30, fourZero: 21.80, fourOne: 21.40, fourTwo: 42.10, fiveZero: 51.60, fiveOne: 50.90, sixZero: 143.00 },
+  { id: "livbre", home: "liv", away: "bre", winner: 66, oneZero: 8.55, twoZero: 8.15, twoOne: 7.96, threeZero: 11.50, threeOne: 11.30, threeTwo: 22.30, fourZero: 21.80, fourOne: 21.40, fourTwo: 42.10, fiveZero: 51.60, fiveOne: 50.90, sixZero: 143.00 },
+  { id: "livwol", home: "liv", away: "wol", winner: 66, oneZero: 8.55, twoZero: 8.15, twoOne: 7.96, threeZero: 11.50, threeOne: 11.30, threeTwo: 22.30, fourZero: 21.80, fourOne: 21.40, fourTwo: 42.10, fiveZero: 51.60, fiveOne: 50.90, sixZero: 143.00 },
+  { id: "livlee", home: "liv", away: "lee", winner: 66, oneZero: 8.55, twoZero: 8.15, twoOne: 7.96, threeZero: 11.50, threeOne: 11.30, threeTwo: 22.30, fourZero: 21.80, fourOne: 21.40, fourTwo: 42.10, fiveZero: 51.60, fiveOne: 50.90, sixZero: 143.00 },
+  { id: "liveve", home: "liv", away: "eve", winner: 66, oneZero: 8.55, twoZero: 8.15, twoOne: 7.96, threeZero: 11.50, threeOne: 11.30, threeTwo: 22.30, fourZero: 21.80, fourOne: 21.40, fourTwo: 42.10, fiveZero: 51.60, fiveOne: 50.90, sixZero: 143.00 },
 
-  // winner: 46.3 - Liverpool home vs Sunderland/Burnley
-  { id: "livsun", home: "liv", away: "sun", winner: 46.3, oneZero: 16.6, twoZero: 4.43, twoOne: 7.3, threeZero: 3.98, threeOne: 6.8, threeTwo: 8.5, fourZero: 4.84, fourOne: 8.2, fourTwo: 12.1, fiveZero: 7.16, fiveOne: 10.8 },
-  { id: "livbur", home: "liv", away: "bur", winner: 46.3, oneZero: 16.6, twoZero: 4.43, twoOne: 7.3, threeZero: 3.98, threeOne: 6.8, threeTwo: 8.5, fourZero: 4.84, fourOne: 8.2, fourTwo: 12.1, fiveZero: 7.16, fiveOne: 10.8 },
+  // winner: 46 - Liverpool home vs Sunderland/Burnley
+  { id: "livsun", home: "liv", away: "sun", winner: 46, oneZero: 8.55, twoZero: 8.15, twoOne: 7.96, threeZero: 11.50, threeOne: 11.30, threeTwo: 22.30, fourZero: 21.80, fourOne: 21.40, fourTwo: 42.10, fiveZero: 51.60, fiveOne: 50.90, sixZero: 143.00 },
+  { id: "livbur", home: "liv", away: "bur", winner: 46, oneZero: 8.55, twoZero: 8.15, twoOne: 7.96, threeZero: 11.50, threeOne: 11.30, threeTwo: 22.30, fourZero: 21.80, fourOne: 21.40, fourTwo: 42.10, fiveZero: 51.60, fiveOne: 50.90, sixZero: 143.00 },
 
   // winner: 229 - Liverpool home vs Aston Villa/Tottenham
-  { id: "livast", home: "liv", away: "ast", winner: 229, oneZero: 16, twoZero: 3.97, twoOne: 6.06, threeZero: 4.0, threeOne: 6.3, threeTwo: 8.0, fourZero: 5.08, fourOne: 8.6, fourTwo: 12.5, fiveZero: 8.9, fiveOne: 11.5 },
-  { id: "livtot", home: "liv", away: "tot", winner: 229, oneZero: 16, twoZero: 3.97, twoOne: 6.06, threeZero: 4.0, threeOne: 6.3, threeTwo: 8.0, fourZero: 5.08, fourOne: 8.6, fourTwo: 12.5, fiveZero: 8.9, fiveOne: 11.5 },
+  { id: "livast", home: "liv", away: "ast", winner: 228, oneZero: 8.05, twoZero: 8.45, twoOne: 7.85, threeZero: 13.10, threeOne: 12.30, threeTwo: 23.20, fourZero: 27.50, fourOne: 25.90, fourTwo: 48.70, fiveZero: 70.60, fiveOne: 66.80, sixZero: 228.00 },
+  { id: "livtot", home: "liv", away: "tot", winner: 228, oneZero: 8.05, twoZero: 8.45, twoOne: 7.85, threeZero: 13.10, threeOne: 12.30, threeTwo: 23.20, fourZero: 27.50, fourOne: 25.90, fourTwo: 48.70, fiveZero: 70.60, fiveOne: 66.80, sixZero: 228.00 },
 
   // winner: 144 - Liverpool home vs Brighton/Nottingham
-  { id: "livbha", home: "liv", away: "bha", winner: 144, oneZero: 16, twoZero: 4.21, twoOne: 6.63, threeZero: 3.97, threeOne: 6.4, threeTwo: 8.1, fourZero: 4.93, fourOne: 8.3, fourTwo: 12.0, fiveZero: 8.01, fiveOne: 10.9 },
-  { id: "livnot", home: "liv", away: "not", winner: 144, oneZero: 16, twoZero: 4.21, twoOne: 6.63, threeZero: 3.97, threeOne: 6.4, threeTwo: 8.1, fourZero: 4.93, fourOne: 8.3, fourTwo: 12.0, fiveZero: 8.01, fiveOne: 10.9 },
+  { id: "livbha", home: "liv", away: "bha", winner: 143, oneZero: 8.55, twoZero: 8.15, twoOne: 7.96, threeZero: 11.50, threeOne: 11.30, threeTwo: 22.30, fourZero: 21.80, fourOne: 21.40, fourTwo: 42.10, fiveZero: 51.60, fiveOne: 50.90, sixZero: 143.00 },
+  { id: "livnot", home: "liv", away: "not", winner: 143, oneZero: 8.55, twoZero: 8.15, twoOne: 7.96, threeZero: 11.50, threeOne: 11.30, threeTwo: 22.30, fourZero: 21.80, fourOne: 21.40, fourTwo: 42.10, fiveZero: 51.60, fiveOne: 50.90, sixZero: 143.00 },
 
-  // winner: 262 - Liverpool away vs West Ham/Brentford/Wolves/Leeds/Everton
-  { id: "whuliv", home: "whu", away: "liv", winner: 262, oneZero: 18, twoZero: 4.21, twoOne: 5.86, threeZero: 3.83, threeOne: 6.0, threeTwo: 7.6, fourZero: 5.17, fourOne: 8.8, fourTwo: 12.6, fiveZero: 8.34, fiveOne: 11.3 },
-  { id: "breliv", home: "bre", away: "liv", winner: 262, oneZero: 18, twoZero: 4.21, twoOne: 5.86, threeZero: 3.83, threeOne: 6.0, threeTwo: 7.6, fourZero: 5.17, fourOne: 8.8, fourTwo: 12.6, fiveZero: 8.34, fiveOne: 11.3 },
-  { id: "wolliv", home: "wol", away: "liv", winner: 262, oneZero: 18, twoZero: 4.21, twoOne: 5.86, threeZero: 3.83, threeOne: 6.0, threeTwo: 7.6, fourZero: 5.17, fourOne: 8.8, fourTwo: 12.6, fiveZero: 8.34, fiveOne: 11.3 },
-  { id: "leeliv", home: "lee", away: "liv", winner: 262, oneZero: 18, twoZero: 4.21, twoOne: 5.86, threeZero: 3.83, threeOne: 6.0, threeTwo: 7.6, fourZero: 5.17, fourOne: 8.8, fourTwo: 12.6, fiveZero: 8.34, fiveOne: 11.3 },
-  { id: "eveliv", home: "eve", away: "liv", winner: 262, oneZero: 18, twoZero: 4.21, twoOne: 5.86, threeZero: 3.83, threeOne: 6.0, threeTwo: 7.6, fourZero: 5.17, fourOne: 8.8, fourTwo: 12.6, fiveZero: 8.34, fiveOne: 11.3 },
+  // winner: 260 - Liverpool away vs West Ham/Brentford/Wolves/Leeds/Everton
+  { id: "whuliv", home: "whu", away: "liv", winner: 260, oneZero: 7.16, twoZero: 7.16, twoOne: 7.23, threeZero: 10.80, threeOne: 11.00, threeTwo: 22.40, fourZero: 21.70, fourOne: 21.70, fourTwo: 44.80, fiveZero: 54.20, fiveOne: 54.20, sixZero: 166.00 },
+  { id: "breliv", home: "bre", away: "liv", winner: 260, oneZero: 7.16, twoZero: 7.16, twoOne: 7.23, threeZero: 10.80, threeOne: 11.00, threeTwo: 22.40, fourZero: 21.70, fourOne: 21.70, fourTwo: 44.80, fiveZero: 54.20, fiveOne: 54.20, sixZero: 166.00 },
+  { id: "wolliv", home: "wol", away: "liv", winner: 260, oneZero: 7.16, twoZero: 7.16, twoOne: 7.23, threeZero: 10.80, threeOne: 11.00, threeTwo: 22.40, fourZero: 21.70, fourOne: 21.70, fourTwo: 44.80, fiveZero: 54.20, fiveOne: 54.20, sixZero: 166.00 },
+  { id: "leeliv", home: "lee", away: "liv", winner: 260, oneZero: 7.00, twoZero: 7.81, twoOne: 6.68, threeZero: 12.80, threeOne: 11.00, threeTwo: 18.80, fourZero: 28.60, fourOne: 24.00, fourTwo: 41.60, fiveZero: 78.10, fiveOne: 68.30, sixZero: 260.00 },
+  { id: "eveliv", home: "eve", away: "liv", winner: 260, oneZero: 7.16, twoZero: 7.16, twoOne: 7.23, threeZero: 10.80, threeOne: 11.00, threeTwo: 22.40, fourZero: 21.70, fourOne: 21.70, fourTwo: 44.80, fiveZero: 54.20, fiveOne: 54.20, sixZero: 166.00 },
 
   // winner: 168 - Liverpool away vs Burnley/Sunderland
-  { id: "burliv", home: "bur", away: "liv", winner: 168, oneZero: 15.3, twoZero: 4.19, twoOne: 5.96, threeZero: 3.86, threeOne: 6.1, threeTwo: 7.8, fourZero: 5.27, fourOne: 8.9, fourTwo: 12.8, fiveZero: 8.46, fiveOne: 11.6 },
-  { id: "sunliv", home: "sun", away: "liv", winner: 168, oneZero: 15.3, twoZero: 4.19, twoOne: 5.96, threeZero: 3.86, threeOne: 6.1, threeTwo: 7.8, fourZero: 5.27, fourOne: 8.9, fourTwo: 12.8, fiveZero: 8.46, fiveOne: 11.6 }
+  { id: "burliv", home: "bur", away: "liv", winner: 166, oneZero: 8.78, twoZero: 6.52, twoOne: 10.00, threeZero: 7.36, threeOne: 11.20, threeTwo: 34.20, fourZero: 11.00, fourOne: 16.60, fourTwo: 50.90, fiveZero: 20.40, fiveOne: 31.70, sixZero: 45.90 },
+  { id: "sunliv", home: "sun", away: "liv", winner: 166, oneZero: 7.16, twoZero: 7.16, twoOne: 7.23, threeZero: 10.80, threeOne: 11.00, threeTwo: 22.40, fourZero: 21.70, fourOne: 21.70, fourTwo: 44.80, fiveZero: 54.20, fiveOne: 54.20, sixZero: 166.00 }
 ];
